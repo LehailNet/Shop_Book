29d4d8c8a0bf2fd12d54f530701b28b49bf5c983
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-04 16:27:32 +0300

    create BookRepository

--- domain/StoreBook/IBookRepository.cs
+++ domain/StoreBook/IBookRepository.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace StoreBook
 {
-    internal interface IBookRepository
+    public interface IBookRepository
     {
+        public Book[] GetAllByTitle(string titlePart);
     }
 }
---
+++ infrastructure/StoreBook.Memory/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace StoreBook.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new Book[]
+        {
+            new Book(1, "First book"),
+            new Book(2, "Second book"),
+            new Book(3, "Thread book")
+        };
+        
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(books => book.Title.Contains(titlePart))
+                .ToArray();
+        }
+    }
+}
